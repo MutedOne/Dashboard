785d4fe08ce66dc8166df382e36de4555c853226
MutedOne <47779436+MutedOne@users.noreply.github.com>
2025-05-28 18:16:40 +0800

    Use Signals and more
    *Use of signal than event emitter
    * Fix bug on responsiveness

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, HostListener, Output } from '@angular/core';
+import { ToogleSignals } from '../../signals/toggleSignals';
 
 @Component({
   selector: 'app-header',
@@ -8,6 +9,27 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   isActiveSearch:boolean = true
+  isActiveGroupNav:boolean = true
+  screenWidth: number = 0;
+ 
+
+  constructor(public toggleSignals: ToogleSignals){
+
+  }
+  ngOnInit() {
+    
+  }
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.screenWidth=(event.target as Window).innerWidth
+    
+    if(this.screenWidth>=992){
+      this.isActiveGroupNav = true;
+    }else{
+      this.isActiveGroupNav = false;
+    }
+    
+  }
 
   checkActive(){
     if(this.isActiveSearch == true){
@@ -16,5 +38,17 @@ export class HeaderComponent {
     }else{
       this.isActiveSearch = true
     }
+  }
+
+  toggleSidenavmobile() {
+   
+    this.toggleSignals.isMobileToggleSidebar()
+  }
+
+  checkToggleMainSidenav(){
+   return this.toggleSignals.getIsSidebar()
+  }
+   toggleMainSidenav(){
+    this.toggleSignals.isToggleSidebar()
   }
 }
